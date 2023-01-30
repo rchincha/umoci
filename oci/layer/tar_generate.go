4ecd060deb380333e1e31c657eb8d1b1db4fa33b
Ramkumar Chinchani <rchincha@cisco.com>
2023-01-30 18:34:10 +0000

    zot: add a experimental patch

--- oci/layer/tar_generate.go
+++ oci/layer/tar_generate.go
@@ -217,7 +217,8 @@ func (tg *tarGenerator) AddFile(name, path string) error {
 			// XXX: I'm not sure if we're unprivileged whether Lgetxattr can
 			//      fail with EPERM. If it can, we should ignore it (like when
 			//      we try to clear xattrs).
-			return errors.Wrapf(err, "get xattr: %s", name)
+			log.Warnf("unable to get xattr: %s", name)
+			continue
 		}
 		// https://golang.org/issues/20698 -- We don't just error out here
 		// because it's not _really_ a fatal error. Currently it's unclear
