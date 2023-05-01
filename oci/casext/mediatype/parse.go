144cb04a0ae429bb4e04559ef85ec27ff3f5d94d
Ramkumar Chinchani <rchincha@cisco.com>
2023-05-01 18:48:03 +0000

    feat!: migrate to image-spec v1.1.0-rc3
    BREAKING CHANGE: image-spec v1.1.0-rc3 drops support for the "artifact
    manifest" media-type

--- oci/casext/mediatype/parse.go
+++ oci/casext/mediatype/parse.go
@@ -192,21 +192,6 @@ func manifestParser(rdr io.Reader) (interface{}, error) {
 	return manifest.Manifest, nil
 }
 
-func artifactParser(rdr io.Reader) (interface{}, error) {
-	var artifactManifest ispec.Artifact
-
-	if rdr != nil {
-		if err := json.NewDecoder(rdr).Decode(&artifactManifest); err != nil {
-			return nil, err
-		}
-	}
-	if artifactManifest.MediaType != "" && artifactManifest.MediaType != ispec.MediaTypeArtifactManifest {
-		return nil, errors.Errorf("invalid artifact manifest detected: artifact manifest contained incorrect mediaType: %s", artifactManifest.MediaType)
-	}
-
-	return artifactManifest, nil
-}
-
 // Register the core image-spec types.
 func init() {
 	RegisterParser(ispec.MediaTypeDescriptor, CustomJSONParser(ispec.Descriptor{}))
@@ -215,5 +200,4 @@ func init() {
 
 	RegisterTarget(ispec.MediaTypeImageManifest)
 	RegisterParser(ispec.MediaTypeImageManifest, manifestParser)
-	RegisterParser(ispec.MediaTypeArtifactManifest, artifactParser)
 }
